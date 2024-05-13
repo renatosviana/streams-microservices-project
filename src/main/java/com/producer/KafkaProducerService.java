a243b2cbe27f93dfc753f330d5efa4c17a5a0601
rviana <renatosviana@gmail.com>
2024-05-13 07:23:01 -0400

    Removing comments from KafkaProducerService.

--- src/main/java/com/producer/KafkaProducerService.java
+++ src/main/java/com/producer/KafkaProducerService.java
@@ -7,26 +7,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducerService {
-//    @Autowired
-//    private KafkaTemplate<String, User> kafkaTemplate;
 
     @Autowired
     private KafkaTemplate<String, User> kafkaTemplate;
 
-
-//    public void sendUser(String topic, User user) {
-//        kafkaTemplate.send(topic, user);
-//    }
-
-
     public void sendUser(String topic, User user) {
         kafkaTemplate.send(topic, user);
     }
-
-//    public void sendCalculationResult(String result) {
-//        kafkaTemplate.send("calculation-results", result);
-//    }
-
 
 }
 
